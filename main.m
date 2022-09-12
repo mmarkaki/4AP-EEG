25b48406f10bd4bb36144be8ed93d03237d709cb
mmarkaki <10400691+mmarkaki@users.noreply.github.com>
2022-09-12 15:38:07 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,3 +1,28 @@
+#start from raw fluoresence files, where the number of frames per segment has been stored in a vector for ease of processing:
+nframes_seg=[8000,8000,8001,8001,32004,32004,32004,32004,32004];
+
+#also the framePeriod stands for the duration of a calcium frame in seconds, it differs per mouse and per segment; these values also
+#can be read from xml relevant files and stored in vectors to load:
+
+#Relevant preprocessing includes putting the segments in their correct order for mice 4 JL65, 5 JL66, JL06VIP and JL07VIP.
+#Also mice 12 and 13 need a proper selection of cells out of provided ROIs; here, we suppose that these preprocessing steps have been
+#already performed hence we can simply load the corrected raw fluoresence files and proceed in the same way for all mice automatically.
+
+#Load raw fluoresence file f, fV:
+
+[nframes,cellnum]=size(f);
+[nframesV,cellnumV]=size(fV);
+
+#df/f from raw fluoresence files is called dff; it is estimated per segment:
+
+
+
+
+# z scored df/f is called newdff:
+
+#Definition of threshold thresh from vehicle:
+
+#Local plateaus definition based on  z scored df/f and  threshold defined from vehicle as:
 nframes=nsamples
 forConcat=100;
 dffV=newdff;
