56b9d8b09f2cea5c597506cdae527e6b01a04832
mmarkaki <10400691+mmarkaki@users.noreply.github.com>
2022-11-23 11:25:38 +0200

    Update ecdfRecruitDisengageFig.m

--- ecdfRecruitDisengageFig.m
+++ ecdfRecruitDisengageFig.m
@@ -1,54 +1,54 @@
-clear all
-close all
-mouse_id=7;
-k=1
-forConcatP=100;
-forConcatV=100;
-load(sprintf('E:/matfiles/mouse%d/4AP/neuronbLocalPlateaus.mat',mouse_id))
-load(sprintf('E:/matfiles/mouse%d/4AP/pop_plateaus_start_forConcatV%d_forConcatP%d_k%d.mat',mouse_id,forConcatV,forConcatP,k))%,'plateau_start')
-load(sprintf('E:/matfiles/mouse%d/4AP/pop_plateaus_end_forConcatV%d_forConcatP%d_k%d.mat',mouse_id,forConcatV,forConcatP,k))%,'plateau_end')
+function [] = ecdf_recruit_disengage(mouse_id,k,plateau_start,plateau_end,neuron,flag)
+%if flag = 1 recruitment; if flag = 0 disengagement
+%clear all
+%close all
+%mouse_id=7;
+%k=1
+%forConcatP=100;
+%forConcatV=100;
+%load(sprintf('E:/matfiles/mouse%d/4AP/neuronbLocalPlateaus.mat',mouse_id))
+%load(sprintf('E:/matfiles/mouse%d/4AP/pop_plateaus_start_forConcatV%d_forConcatP%d_k%d.mat',mouse_id,forConcatV,forConcatP,k))%,'plateau_start')
+%load(sprintf('E:/matfiles/mouse%d/4AP/pop_plateaus_end_forConcatV%d_forConcatP%d_k%d.mat',mouse_id,forConcatV,forConcatP,k))%,'plateau_end')
 
-neuron_med=neuron;
-cellnum=length(neuron_med);
-clear neuron
+cellnum=length(neuron);
 onsets=[];
 offsets=[];
-Rsquares_med=[];
-glob_med=[];
-Amplitds_med=[];
-lag_med=[];%Rmax/2 / slope
-lag_glob_med=[];
-lag2_med=[];%X50_med-(onset-start)
-SlopeDeriv_med=[];
-neuron_rec_med=[];
-neuron_rec_med2=[];
-plat_rec_med=[];
+Rsquares=[];
+glob=[];
+Amplitds=[];
+%lag_med=[];%Rmax/2 / slope
+%lag_glob_med=[];
+%lag2_med=[];%X50_med-(onset-start)
+SlopeDeriv=[];
+neuron_rec=[];
+neuron_rec2=[];
+plat_rec=[];
 rsqS=0.8
 
 for n=1:cellnum%separate the properly fitted values based on Rsquare first:
-         j=find(neuron_med(n).rsqSigm>=rsqS);%140
-         neuron_rec_med=[neuron_rec_med length(neuron_med(n).start(j))];
+         j=find(neuron(n).rsqSigm>=rsqS);%140
+         neuron_rec=[neuron_rec length(neuron(n).start(j))];
          for jj=1:length(j)
-             neuron_rec_med2=[neuron_rec_med2 n];%neuron n appears length(j) times
+             neuron_rec2=[neuron_rec2 n];%neuron n appears length(j) times
          end
-         plat_rec_med=[plat_rec_med neuron_med(n).start(j)];
-         Rsquares_med=[Rsquares_med neuron_med(n).rsqSigm(j)];
-         glob_med=[glob_med neuron_med(n).glob(j)];%the global plateaus
-         Amplitds_med=[Amplitds_med neuron_med(n).ZmaxSG(j)];
-         Rmax=neuron_med(n).ZmaxSG(j);
-         temp= tan(neuron_med(n).slopeDeriv(j));%df/f z score per frames
-         SlopeDeriv_med=[SlopeDeriv_med temp]; 
-         lag=(Rmax/2)./temp;%slope;%
-         lag_med=[lag_med lag];%Rmax/2 / slope gives one option of x50
-         x50=neuron_med(n).x50True(j);
-         onset=neuron_med(n).plateau_start(j)';%I need this
-         offset=neuron_med(n).plateau_end(j)';%I need this too
+         plat_rec=[plat_rec neuron(n).start(j)];
+         Rsquares=[Rsquares neuron(n).rsqSigm(j)];
+         glob=[glob neuron(n).glob(j)];%the global plateaus
+         Amplitds=[Amplitds neuron(n).ZmaxSG(j)];
+         Rmax=neuron(n).ZmaxSG(j);
+         temp= tan(neuron(n).slopeDeriv(j));%df/f z score per frames
+         SlopeDeriv=[SlopeDeriv temp]; 
+         %lag=(Rmax/2)./temp;%slope;%
+         %lag_med=[lag_med lag];%Rmax/2 / slope gives one option of x50
+         %x50=neuron(n).x50True(j);
+         onset=neuron(n).plateau_start(j)';%I need this
+         offset=neuron(n).plateau_end(j)';%I need this too
          onsets=[onsets onset];%starts
          offsets=[offsets offset];
-         start=neuron_med(n).start(j);
-         lag2=x50-(onset-start);%this is more correct?
-         lag2_med=[lag2_med lag2./33];%
-         lag_glob_med=[lag_glob_med x50+start];%absolute time to half Rmax;
+         %start=neuron_med(n).start(j);
+         %lag2=x50-(onset-start);%this is more correct?
+         %lag2_med=[lag2_med lag2./33];%
+         %lag_glob_med=[lag_glob_med x50+start];%absolute time to half Rmax;
 end
 
 %FOV plots of parameters in bursts:
@@ -77,37 +77,38 @@ bigstart=[];
 biglagC=[];
 burstID=[];
 neuronsID=[];
-onsetsCor=[];
+onfsetsCor=[];
 Y50s=[];
-%offset4=[];
 figure
 hold on
 for ib=1:nbursts
-    jj=find(glob_med==ib);%
+    jj=find(glob==ib);%
     tot_neurons=[tot_neurons length(jj)];
-    %load(sprintf('results/mouse7/Bursts/burst%d_lags.mat',ib));
     if (length(jj)>3)%at least 4 neurons with local plateaus in this burst
         %in a composite burst, which are many unimodal bursts very close together,
         %a neuron might be recruited later in the composite burst, 
         %but first in a later unimodal burst
-        temp=neuron_rec_med2(jj);%these are the ON neurons
+        temp=neuron_rec2(jj);%these are the ON neurons
         neuronsID=[neuronsID temp];
-        %clag=offsets(jj)./30;%disengagement
-        clag=onsets(jj)./30;%engagement
-        onsetsCor=[onsetsCor onsets(jj)];
-        clag(clag<0)=0;
+        if flag==1
+            clag=onsets(jj);%engagement
+         else
+            clag=offsets(jj);%disengagement
+         end
+         onfsetsCor=[onfsetsCor clag];
         refere=median(clag);
-        Y50s=[Y50s 30*refere*ones(1,length(jj))];%glob meds
+        Y50s=[Y50s refere*ones(1,length(jj))];%glob meds
         %stde=std(clag);%within a burst-relative disengagement
         %stde=max(abs(clag-refere));  
         clagZ=(clag-refere);%./stde;%rr-ratio of neurons in this burst
         biglagC=[biglagC clagZ];%lags values
         burstID=[burstID ib*ones(1,length(jj))];%glob meds
-        %offset7=[offset7 clagZ];%4288
         if length(clagZ)>5
             [y,x]=ecdf(clagZ);
-            plot(x,100*y,'Color',[1, 0, 0, 0.2])%red for recruitment
-            %plot(x,100*y,'Color', [0, 1, 0, 0.2])%blue for disengagegement
+            if flag==1
+               plot(x,100*y,'Color',[1, 0, 0, 0.2])%red for recruitment
+            else
+               plot(x,100*y,'Color', [0, 1, 0, 0.2])%blue for disengagegement
         end
     end
 end
@@ -120,3 +121,4 @@ save(sprintf('matfiles/mouse%d/4AP/mouse%d_burstid.mat',mouse_id,mouse_id),'burs
 save(sprintf('matfiles/mouse%d/4AP/mouse%d_neuronsid.mat',mouse_id,mouse_id),'neuronsID')
 save(sprintf('matfiles/mouse%d/4AP/mouse%d_onsets.mat',mouse_id,mouse_id),'onsetsCor')
 save(sprintf('matfiles/mouse%d/4AP/mouse%d_Y50sec.mat',mouse_id,mouse_id),'Y50s')
+end
