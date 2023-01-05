65f49581eeaf0e1d8dce4f1aab7f4af7c7e71041
mmarkaki <10400691+mmarkaki@users.noreply.github.com>
2023-01-05 15:36:36 +0200

    Update EEGplot.m

--- EEGplot.m
+++ EEGplot.m
@@ -73,3 +73,256 @@ for iseg=1:seg_num
         close
     end
 end 
+
+%PSD: Estimate power spectral density via the multitaper method using Slepian or sine tapers.
+%PSD everywhere, the whole EEG:17 sec in every segment of 18 minutes means 
+% 64 sliding windows of 20 sec with 3 sec overlap:
+half_sec=0.5*Fs
+nwind = 64
+n=30000;%3 sec sliding window; overlap by 2.5 sec or 25000 frames
+p=25000;
+bin=n-p%half a sec in EEG frames
+binCa=(bin/Fs)./framePeriods%half a sec in calcium frames
+half_sec=n-p%5000 EEG frames per half sec
+nseg=seg_num
+for iseg=1:nseg 
+    tstartEEG0=1+(iseg-1)*seg_frames;%EEG counter  time started from zero
+    tendEEG0=iseg*seg_frames%
+    tstartCa0=1+(iseg-1)*seg_framesCa;%EEG counter  time started from zero
+    tendCa0=iseg*seg_framesCa%yinitial1=eeg
+    %buffers now:    for jw=1:nwind
+    onsetEEG=eeg4AP.CleanEEG_cV(tstartEEG0+half_sec:tendEEG0-half_sec,1);
+    [buffer1,z]=buffer(onsetEEG,n,p);%30000 *2193  windows
+    buffer2=buffer1(:,6:end);%28
+    nPSDs=size(buffer2,2);%just plot this, the rest is empty
+    matrixOnset=zeros(nfreqs,nPSDs);% 
+    confid=zeros(nfreqs,2,nPSDs);
+    for metr=1:nPSDs
+        signs=buffer2(:,metr);
+        y1=filter(b,a,signs);
+            %computes the multitaper PSD estimate at the frequencies specified in frqs which have been estimated as following:
+%             fb = cwtfilterbank('SignalLength',length(y1),'SamplingFrequency',Fs,'FrequencyLimits',[minfreq maxfreq], 'Wavelet',"amor")          
+%             [cfs, frqs] = wt(fb,y1);%67 frequencies
+        [pxx,frqs,Pxxc]=pmtm(y1,3.5,frqs,Fs,'ConfidenceLevel',0.95);%,'freqrange');%'unity','centered');
+        matrixOnset(:,metr)=pxx;%mean(interp_cfs2,2);
+        confid(:,:,metr)=Pxxc;
+     end
+     save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/matrixOnsetSeg%d_multitaper.mat',mouse_id,iseg),'matrixOnset');
+     save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/confidOnsetSeg%d_multitaper.mat',mouse_id,iseg),'confid');
+     %random from the ones already estimated
+end
+
+xxx=[ceil(plateau_start/seg_framesCa)     ceil(plateau_end/seg_framesCa)]
+%mouse_id=6;
+%bin=5000;
+%PSD plus random estimation onsets:
+yinitial=eeg4AP.CleanEEG_cV(:,1);
+for iseg=1:nseg    
+    tstartEEG0=1+(iseg-1)*seg_frames;%EEG counter  time started from zero
+    tendEEG0=iseg*seg_frames%
+    tstartCa0=1+(iseg-1)*seg_framesCa;%EEG counter  time started from zero
+    tendCa0=iseg*seg_framesCa;
+    lips=find(xxx(:,1)==iseg);
+    for ipp=1:length(lips)
+        ip=lips(ipp);%ip2=lips2(ipp);
+        temp=0;
+        if iseg>1
+            temp=EEGsegSec*(iseg-1)-sum(CaSeg(1:iseg-1));
+        end
+        tstart=temp*Fs+plateau_start(ip)*framePeriods(iseg)*Fs;%337);   
+        tstartEEG=round(tstart)-20*bin+1;%bin is half a second
+        tstartCa=round(plateau_start(ip))-round(20*binCa(iseg))+1;
+        if tstartCa<0 %then also tstartEEG<0
+            tstartCa=tstartCa0;%segment start
+            tstartEEG=tstartEEG0;
+        end    
+        t_endEEG=round(tstart+20*bin);
+        t_endCa=round(plateau_start(ip)+20*binCa(iseg));
+        %13th and 23rd burst end at end of segments
+        if t_endCa > tendCa0
+            t_endCa=tendCa0;%segment end
+            t_endEEG=tendEEG0;
+        end
+        %here newer code loads results:
+        onsetEEG=eeg4AP.CleanEEG_cV(tstartEEG:t_endEEG,1);
+        %load('E:/matfiles/mouse13/4AP/zscoreMeanSJL13Cells.mat')
+        %onsetCa=zcorrectC(tstartCa:t_endCa);%498
+        [buffer1,z]=buffer(onsetEEG,n,p);%30000x33, first 5 contain zeros; omit them;
+        buffer2=buffer1(:,6:end);%28
+        nPSDs=size(buffer2,2);%48/35- just plot this, the rest is empty
+        matrixOnset=zeros(nfreqs,nPSDs);% matrixOnset2=zeros(90,nPSDs);
+        confid=zeros(nfreqs,2,nPSDs);
+        for metr=1:nPSDs
+            signs=buffer2(:,metr);
+            y1=filter(b,a,signs);
+            %computes the multitaper PSD estimate at the frequencies specified in f
+%             fb = cwtfilterbank('SignalLength',length(y1),'SamplingFrequency',Fs,'FrequencyLimits',[minfreq maxfreq], 'Wavelet',"amor")          
+%             [cfs, frqs] = wt(fb,y1);%67 frequencies
+            [pxx,frqs,Pxxc]=pmtm(y1,3.5,frqs,Fs,'ConfidenceLevel',0.95);%,'freqrange');%'unity','centered');
+            matrixOnset(:,metr)=pxx;%mean(interp_cfs2,2);
+            confid(:,:,metr)=Pxxc;
+        end
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/matrixOnsetSeg%dBurst%d_multitaper.mat',mouse_id,iseg,ip),'matrixOnset');
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/confidOnsetSeg%dBurst%d_multitaper.mat',mouse_id,iseg,ip),'confid');
+        %before b
+%       %circular shift for null distribution:
+        r=randi([1 length(yinitial)],30,1);
+        RandmatrixOnset=zeros(length(frqs),nPSDs,30);
+        for mmetr=1:30
+            K=r(mmetr);
+            yfinal=circshift(yinitial,K);
+            x1=yfinal(tstartEEG:t_endEEG);%
+            z=[];
+            [buffer1,z]=buffer(x1,n,p);
+            %omit the first 5 
+            buffer2=buffer1(:,6:end);
+            nPSDs=size(buffer2,2);%28
+            for metr=1:nPSDs
+                signs=buffer2(:,metr);
+                %signsV=buffer2V(:,metr);
+                y1=filter(b,a,signs);%y1 = filter(b,a,x);       
+                [pxx,frqs]=pmtm(y1,3.5,frqs,Fs);%,'freqrange');%'unity','centered');
+                RandmatrixOnset(:,metr,mmetr)=pxx;
+             end
+        end
+        meanRandmatrixOnset=mean(RandmatrixOnset,3);
+        stdRandmatrixOnset=std(RandmatrixOnset,0,3);
+        semRandmatrixOnset=std(RandmatrixOnset,0,3)/sqrt(length(RandmatrixOnset));
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/meanRandom_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip),'meanRandmatrixOnset');
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/stdRandom_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip),'stdRandmatrixOnset');
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/semRandom_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip),'semRandmatrixOnset');
+        %I could just use the same with first 4-5 plateau without estimating them for every
+        %plateau; just pick them at random!
+%         load(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/meanRandom_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip))
+%         load(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/stdRandom_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip))
+        z_score=(matrixOnset-meanRandmatrixOnset)./stdRandmatrixOnset;
+        save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Onsets/zscore_%dSeg_matrixOnset_burst%d.mat',mouse_id,iseg,ip),'z_score');
+        %gather z scores and keep the distributions from distance from end:
+        %figures:
+        figure('DefaultAxesFontSize',14)
+        hold on
+        imagesc([-19:20],frqs,z_score)%of onset/offset
+        ylabel('Frequency (Hz)')
+%         yticks([0 1 2])
+%         yticklabels({'1','10','100'})
+        set(gca,'YDir','normal') 
+        colorbar%'PSD (dB/Hz)'
+        shading interp; % interpolate shading to make color transitions visually smooth
+        title(sprintf('burst %d onset PSD',ip))
+        axis tight
+        saveas(gcf,sprintf('E:/figures/mouse%d/4AP/EEG/Buffers/Onsets/burst%d_PSDonset.png',mouse_id,ip));
+        close
+    end
+end
+%repeat for offsets:
+ nseg=seg_num
+    for iseg=1:nseg 
+        tstartEEG0=1+(iseg-1)*seg_frames;%EEG counter  time started from zero
+        tendEEG0=iseg*seg_frames%
+        tstartCa0=1+(iseg-1)*seg_framesCa;%EEG counter  time started from zero
+        tendCa0=iseg*seg_framesCa%yinitial1=eegData.CleanEEG_cV(tstart0:tend0, 1);%whole segment   
+        lips=find(xxx(:,2)==iseg)% %around onset and around offset:
+        for ipp=1:length(lips)%1:length(plateau_start)%segments 8,10 without bursts
+            ip=lips(ipp);%ip2=lips2(ipp);
+            temp=0;
+            if iseg>1
+                temp=EEGsegSec*(iseg-1)-sum(CaSeg(1:iseg-1))
+            end
+            tstart=temp*Fs+plateau_end(ip)*framePeriods(iseg)*Fs;%337);%=round(((iseg-1)*(-CaSeg+EEGseg)+plateau_end(ip))*337);    
+            %=round(((iseg-1)*(-CaSeg+EEGseg)+plateau_end(ip))*337);    
+            tstartEEG=round(tstart)-20*bin+1;%bin is half a second
+            tstartCa=plateau_end(ip)-round(20*binCa(iseg))+1;
+            if tstartCa<0 %then also tstartEEG<0
+                tstartCa=tstartCa0;%segment start
+                tstartEEG=tstartEEG0;
+            end    
+            t_endEEG=round(tstart+20*bin);
+            t_endCa=plateau_end(ip)+round(20*binCa);
+            %13th and 23rd burst end at end of segments
+            if t_endCa > tendCa0
+                t_endCa=tendCa0;%segment end
+                t_endEEG=tendEEG0;
+            end
+            %here newer code loads results:
+            offsetEEG=eeg4AP.CleanEEG_cV(tstartEEG:t_endEEG);%167737/337=497.7359
+            offsetCa=zcorrectC(tstartCa:t_endCa);%498
+            [buffer1,z]=buffer(offsetEEG,n,p);%30000x33, first 5 contain zeros; omit them;
+            buffer2=buffer1(:,6:end);%28
+            nPSDs=size(buffer2,2);%48/35- just plot this, the rest is empty
+            matrixOffset=zeros(nfreqs,nPSDs);%
+            confid=zeros(nfreqs,2,nPSDs);
+            for metr=1:nPSDs
+                signs=buffer2(:,metr);
+                y1=filter(b,a,signs);
+                [pxx,frqs,Pxxc]=pmtm(y1,3.5,frqs,Fs,'ConfidenceLevel',0.95);%,'freqrange');%'unity','centered');
+                matrixOffset(:,metr)=pxx;%mean(interp_cfs2,2);
+                confid(:,:,metr)=Pxxc;
+            end
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/matrixOffsetSeg%dBurst%d_multitaper.mat',mouse_id,iseg,ip),'matrixOffset');
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/confidOffsetSeg%dBurst%d_multitaper.mat',mouse_id,iseg,ip),'confid');
+            %before b
+            %circular shift for null distribution was done for irrelevant pieces but that's fine:
+            r=randi([1 length(yinitial)],30,1);
+            RandmatrixOffset=zeros(length(frqs),nPSDs,30);
+            for mmetr=1:30
+                K=r(mmetr);
+                yfinal=circshift(yinitial,K);
+                x1=yfinal(tstartEEG:t_endEEG);%
+                z=[];
+                [buffer1,z]=buffer(x1,n,p);
+                %omit the first 5 
+                buffer2=buffer1(:,6:end);
+                nPSDs=size(buffer2,2);%28
+                for metr=1:nPSDs
+                    signs=buffer2(:,metr);
+                    y1=filter(b,a,signs);%y1 = filter(b,a,x);         
+                    [pxx,frqs]=pmtm(y1,3.5,frqs,Fs);%,'freqrange');%'unity','centered');
+                    RandmatrixOffset(:,metr,mmetr)=pxx;
+                end
+            end
+            meanRandmatrixOffset=mean(RandmatrixOffset,3);
+            stdRandmatrixOffset=std(RandmatrixOffset,0,3);
+            semRandmatrixOffset=std(RandmatrixOffset,0,3)/sqrt(length(RandmatrixOffset));
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/meanRandom_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip),'meanRandmatrixOffset');
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/stdRandom_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip),'stdRandmatrixOffset');
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/semRandom_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip),'semRandmatrixOffset');
+            %I could just use the same with first 4-5 plateau without estimating them for every
+            %plateau; just pick them at random!
+    %         load(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/meanRandom_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip));%,'meanRandmatrixOffset');
+    %         load(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/stdRandom_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip));%,'stdRandmatrixOffset');
+            z_score=(matrixOffset-meanRandmatrixOffset)./stdRandmatrixOffset;
+            save(sprintf('E:/matfiles/mouse%d/4AP/Buffers/OnsetsOffsets/Offsets/zscore_%dSeg_matrixOffset_burst%d.mat',mouse_id,iseg,ip),'z_score');
+            %figures:
+            figure('DefaultAxesFontSize',14)
+            hold on
+            imagesc([-19:20],frqs,z_score)%of onset/offset
+            ylabel('Frequency (Hz)')
+    %         yticks([0 1 2])
+    %         yticklabels({'1','10','100'})
+            set(gca,'YDir','normal') 
+            colorbar%'PSD (dB/Hz)'
+            shading interp; % interpolate shading to make color transitions visually smooth
+            title(sprintf('burst %d offset PSD',ip))
+            axis tight
+            saveas(gcf,sprintf('E:/figures/mouse%d/4AP/EEG/Buffers/Offsets/burst%d_PSDoffset.png',mouse_id,ip));
+            close
+        end
+    end
+    %onsets figures loading PSD results:
+    %avrgConfid=nan(nfreqs,2,35,100);
+    interp_frqs = frqs
+delta=find(10.^interp_frqs>=1 & 10.^interp_frqs < 4)%20 frequencies
+theta=find(10.^interp_frqs>=4 & 10.^interp_frqs <8)%10 frequencies
+alpha=find(10.^interp_frqs>=8 & 10.^interp_frqs <12)%6 frequencies
+%beta=10.^interp_frqs(10.^interp_frqs>=12 & 10.^interp_frqs <30)%13 frequencies
+beta=find(10.^interp_frqs>=15 & 10.^interp_frqs <30)%10 frequencies-not between 12 to 15 Hz?
+gamma1=find(10.^interp_frqs>=30 & 10.^interp_frqs <60)%10 frequencies
+gamma1_5=find(10.^interp_frqs>60 & 10.^interp_frqs <=90)%6 frequencies
+gamma2=find(10.^interp_frqs>=60 & 10.^interp_frqs <100)%17 frequencies/200
+%from 10^1.5786=37 Hz up to 10^1.9097=81.23 Hz, filt
+
+avrgOnset=nan(nfreqs,35,100);
+avrgMean=nan(nfreqs,35,100);
+avrgSTD=nan(nfreqs,35,100);
+avrgSEM=nan(nfreqs,35,100);
+avrgZscore=nan(nfreqs,35,100);
